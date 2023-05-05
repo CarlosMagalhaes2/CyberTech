531d461314683aa7519070d741e8e5b4b0ba2f96
Carlos <carlosmagalhaes2002@gmail.com>
2023-05-05 10:06:17 +0100

    Header changes

--- header.php
+++ header.php
@@ -1,73 +1,111 @@
-<nav class="navbar  sticky-top navbar-expand-lg ">
-
-  <div class="container">
-  <a class="navbar-brand margin-top25" href="https://localhost/Cybertech">
-          <img src="imgs/IconLogo2.png" alt="logo" height="80" alt="" />
+<div class="offcanvas offcanvas-start" id="demo">
+  <div class="offcanvas-header">
+    <a class="navbar-brand mx-3" href="/Cybertech">
+      <img src="imgs/IconLogo2White.png" alt="logo" height="55" alt="" />
+    </a>
+    <button type="button" data-bs-theme="dark" class="btn-close btn-close-white text-reset"
+      data-bs-dismiss="offcanvas"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Portáteis
         </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto mr-0">
-        
-        <li class="nav-item mx-2">
-          <div class="dropdown">
-            <button class="btn btn-produtos dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Produtos
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <hr class="dropdown-divider">
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+      <div class="btn-group dropend">
+        <button type="button" class="btn btn-secondary">
+          Split dropend
+        </button>
+        <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          <span class="visually-hidden">Toggle Dropend</span>
+        </button>
+
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <hr class="dropdown-divider">
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </div>
+
+    </ul>
+  </div>
+</div>
+
+
+
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <div class="col-3 d-flex">
+      <button class="btn mx-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-list-nested"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+        </svg>
+      </button>
+      <a class="navbar-brand mx-3" href="/Cybertech">
+        <img src="imgs/IconLogo2.png" alt="logo" height="45" alt="" />
+      </a>
+    </div>
+    <!-- Pesquisa -->
+    <div class="col-6">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="input-group">
+          <input type="text" class="form-control input-lg" id="search-church" placeholder="Pesquisa">
+          <div class="input-group-btn">
+            <button type="submit" class="btn-pesquisa">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+                viewBox="0 0 16 16">
+                <path
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
             </button>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Portáteis'">Portateis</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Desktops'">Desktops</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Componentes'">Componentes</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Smartphones'">Smartphones</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Imagem e Som'">Imagem e Som</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Periféricos'">Periféricos</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Cabos e Acessórios'">Cabos e Acessórios</a></li>
-              <li><a class="dropdown-item" href="categorias.php?Categoria='Consolas'">Consolas</a></li>
-            </ul>
           </div>
-        </li>
-        <li class="nav-item mx-2">
-          <a class="btn btn-produtos" href="servicos.php" role="button">Serviços</a>
-        </li>
-        <li class="nav-item mx-2">
-          <a class="btn btn-produtos" href="contactos.php" role="button">Contactos</a>
-        </li>
-      </ul>
-
+        </div>
+      </div>
     </div>
-
-
-
-    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-      <ul class=" navbar-nav nav-right  mb-2 mb-lg-0">
-        <li class=nav-item mr-2 ml-2>
-          <form class="d-flex form-pesquisa" action="resultadopesquisa.php" method="post" role="search">
-            <input class="form-control me-2" type="search" name="pesquisa" placeholder="Pesquisa" aria-label="Search">
-
-          </form>
-        </li>
-        <li class="nav-item mr-2 ml-4">
-          <a class="btn btn-carrinho" href="carrinho.php" role="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#E7F6F2" class="bi bi-cart-fill icon" viewBox="0 0 16 16">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-            </svg>
-          </a>
-        </li>
-        <li class="nav-item mx-2">
-
-          <a class="btn btn-carrinho" href="/CyberTech/login.php">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill icon" viewBox="0 0 16 16">
-              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-            </svg>
-          </a>
-
-        </li>
-
-
-        </li>
-      </ul>
+    <div class="col d-flex justify-content-end">
+      <!-- Botão Favoritos -->
+      <li class="nav-item ">
+        <a class="btn btn-carrinho" href="carrinho.php" role="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#E7F6F2" class="bi bi-cart-fill icon"
+            viewBox="0 0 16 16">
+            <path
+              d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+          </svg>
+        </a>
+      </li>
+      <!-- Botão Carrinho -->
+      <li class="nav-item ">
+        <a class="btn btn-carrinho" href="carrinho.php" role="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#E7F6F2" class="bi bi-cart-fill icon"
+            viewBox="0 0 16 16">
+            <path
+              d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+          </svg>
+        </a>
+      </li>
+      <!-- User -->
+      <li class="nav-item">
+        <a class="btn btn-carrinho tamanho-user" href="/CyberTech/login.php">
+          Ola, User
+        </a>
+      </li>
     </div>
-
   </div>
 </nav>
