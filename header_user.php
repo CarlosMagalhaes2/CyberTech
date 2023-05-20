d6f85a909163d811b03ebd30fe47b5c0d0bcadf2
Carlos Magalhaes <carlos.magalhaes@itcenter.com>
2023-05-20 15:07:01 +0100

    user

--- header_user.php
+++ header_user.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <div class="offcanvas offcanvas-start" id="demo">
     <div class="offcanvas-header">
-        <a class="navbar-brand mx-3" href="/Cybertech">
+        <a class="navbar-brand mx-3" href="/CyberTech">
             <img src="imgs/IconLogo2White.png" alt="logo" height="55" alt="" />
         </a>
         <button type="button" data-bs-theme="dark" class="btn-close btn-close-white text-reset"
@@ -34,7 +34,7 @@ session_start();
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed tamanho-user" type="button" data-bs-toggle="collapse"
                         data-bs-target="#Desktops" aria-expanded="false" aria-controls="Desktops">
-                        <img src="imgs/icons/desktopsSemCirculo.png" width="35px" class="pd-5"> Desktops
+                        <img src="imgs/icons/DesktopsSemCirculo.png" width="35px" class="pd-5"> Desktops
                     </button>
                 </h2>
                 <div id="Desktops" class="accordion-collapse collapse" data-bs-parent="#menu">
@@ -189,7 +189,7 @@ session_start();
                         d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </button>
-            <a class="navbar-brand mx-3" href="/Cybertech">
+            <a class="navbar-brand mx-3" href="/CyberTech">
                 <img src="imgs/IconLogo2.png" alt="logo" height="45" alt="" />
             </a>
         </div>
